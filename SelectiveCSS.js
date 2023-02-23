5bee69679873232fa3861ce8fc354a44bc23e02a
阿循 <105115552+GANSOONLEE@users.noreply.github.com>
2023-02-23 13:53:18 +0000

    增加 .gitignore

--- SelectiveCSS.js
+++ SelectiveCSS.js
@@ -1,13 +1,22 @@
 class AutoStyle {
-    constructor(name, url, id, start, end, priority) {
+    constructor(name, url, id, start, end, priority){
         this.name = name;
         this.url = url;
         this.id = id;
         this.startDate = new Date(start);
         this.endDate = new Date(end);
 
-        if (this.startDate > this.endDate) {
-            throw new Error('开始日期不能晚于结束日期！');
+        // 使用 XMLHttpRequest 檢查 URL 是否存在
+        function checkFile(url) {
+            var http = new XMLHttpRequest();
+            http.open('HEAD', url, false);
+            http.send();
+            return http.status !== 404;
+        }
+        
+        if (!checkFile(url)) {
+            alert('位於 ' + url + ' 的文件不存在！');
+            return;
         }
 
         this.priority = priority;
@@ -21,23 +30,23 @@ class StyleFactory {
     // 在工廠函數的原型上定義 createAutoStyle 方法
     createAutoStyle(name, url, id, start, end, priority = 0) {
 
-        //創建一個 AutoStyle 實例
+        // 創建一個 AutoStyle 實例
         var autoStyle = new AutoStyle(name, url, id, start, end, priority);
 
-        // 判斷是否有相同id的自動樣式
-        if (!document.querySelector('#' + id)) {
-            var link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.href = url;
-            link.id = id;
+        // 如果參數不符合，則不會創建實例
+        if (AutoStyle) {
+            // 判斷是否有相同id的自動樣式
+            if (!document.querySelector('#' + id)) {
+                var link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = url;
+                link.id = id;
 
-            document.head.appendChild(link);
-        }else{
-            alert("已存在該id的自動樣式！")
+                document.head.appendChild(link);
+            } else {
+                alert("已存在該id的自動樣式！")
+            }
         }
-
-        // 返回 autoStyle 實例
-        return autoStyle;
     }
 }
 
